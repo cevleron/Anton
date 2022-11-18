6a756c89d05237bf6ce81b0f3479285f81cf1353
Anton <Cevleron@yandex.ru>
2022-11-18 08:42:39 +0300

    add rast2

--- Rast02/Program.cs
+++ Rast02/Program.cs
@@ -4,11 +4,13 @@
 
 Console.WriteLine("Введите число от 1 до 7");
 
-int number = Convert.ToInt32(Console.ReadLine());
-if (number = 1) Console.WriteLine("Понелдельник");
-else if (number = 2) Console.WriteLine("Вторник");
-else if (number = 3) Console.WriteLine("Среда");
-else if (number = 4) Console.WriteLine("Четверг");
-else if (number = 5) Console.WriteLine("Пятница");
-else if (number = 6) Console.WriteLine("Суббота");
-else if (number = 7) Console.WriteLine("Воскресенье");
+string number = Console.ReadLine();
+
+if ( number == "1" ) Console.WriteLine("Понелдельник");
+else if ( number == "2" ) Console.WriteLine("Вторник");
+else if ( number == "3" ) Console.WriteLine("Среда");
+else if ( number == "4" ) Console.WriteLine("Четверг");
+else if ( number == "5" ) Console.WriteLine("Пятница");
+else if ( number == "6" ) Console.WriteLine("Суббота");
+else if ( number == "7" ) Console.WriteLine("Воскресенье");
+else Console.WriteLine("Введены некорректные данные");
