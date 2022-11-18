32d9731dc2063ede75e0f7a05e9f97e3eeab21aa
Anton <Cevleron@yandex.ru>
2022-11-18 08:51:38 +0300

    add rast3

--- Rast03/Program.cs
+++ Rast03/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 5. Напишите программу, которая на вход принимает одно целое число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2-> "-2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+if (number >= 0)
+{
+while (count <= number)
+{
+    Console.Write($"{count} ");
+    count++;
+}
+} 
+else Console.Write("Введено некорректное значение");
