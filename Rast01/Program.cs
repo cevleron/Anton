9106815acdf3d71ab84405c67ed75e510924c1a5
Anton <Cevleron@yandex.ru>
2022-11-18 07:36:00 +0300

    Add Rast01

--- Rast01/Program.cs
+++ Rast01/Program.cs
@@ -1,7 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 01. Напишите программу, которая 
+// 1. на вход принимает два числа и 
+// 2. проверяет, является ли первое число квадратом второго.
 
-// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3, b = 9 -> нет
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 == number2 * number2) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
