33202389910d1ea06c214f0daa4dc9817e880c67
Anton <Cevleron@yandex.ru>
2022-11-21 13:15:43 +0300

    add Rast13

--- Rast13/Program.cs
+++ Rast13/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите целое число до 3-х символов включительно");
+long number = Convert.ToInt32(Console.ReadLine());
+
+
+while (number > 999)
+{
+    number /= 10;
+}
+Console.WriteLine(number);
+
+long result = number%10;
+Console.WriteLine(result);
+// if (number < 999) Console.WriteLine("Третьей цифры нет");
+
+// while (number < 99) Console.WriteLine("Третьей цифры нет");
