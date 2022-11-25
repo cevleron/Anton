7a72ddb4309e765cd635af5ee071b36d0b477b34
Anton <Cevleron@yandex.ru>
2022-11-25 13:43:19 +0300

    add Rast18

---
+++ Rast17/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты точки X и Y, 
+// причем X не равен 0 и Y не равен 0 и выдает номер четверти в которой находится эта точка.
+
+
+Console.WriteLine("Введите координаты точки");
+Console.WriteLine("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int Quarter (int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return 1;
+    if (xc < 0 && yc > 0) return 2;
+    if (xc < 0 && yc < 0) return 3;
+    if (xc > 0 && yc < 0) return 4;
+    return 0;
+} 
+int quarter = Quarter(x,y);
+string result = quarter > 0 ? $"Указанные координаты соответствуют четверти - {quarter}"
+: "введены некорректные координаты";
+
+Console.WriteLine(result);
---
+++ Rast18/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон 
+// возможных координат точек в этой четверти(x и y).
+
+Console.WriteLine("Введите номер четверти");
+string Quarter = Console.ReadLine();
+
+
+string Range (string Quarter1)
+    {
+        if (Quarter1 == "1") Console.WriteLine("x > 0, y > 0" );
+        if (Quarter1 == "2") Console.WriteLine("x < 0, y < 0" );
+        if (Quarter1 == "3") Console.WriteLine("x > 0, y < 0" );
+        if (Quarter1 == "4") Console.WriteLine("x < 0, y > 0" );
+        return "Введен некоректная четверть";
+    }
+
+string range = Range(Quarter);
+
+Console.WriteLine(range);
