c9d997ec5b953456612284bd5ec1b170879fdaae
Anton <Cevleron@yandex.ru>
2022-11-28 17:51:38 +0300

    add rast28

--- Rast28/Program.cs
+++ Rast28/Program.cs
@@ -5,3 +5,24 @@
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 
+while (number <= 0)
+{
+    Console.WriteLine("Введите число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+
+    if (number < 0) Console.WriteLine("Введены неверные данные");
+}
+
+int MultDigits(int num)
+{
+    int mult = 1;
+
+    for (int i = 1; i <= num; i++)
+
+        { 
+            mult = mult * i;
+        }
+return mult;
+}   
+int multDigits = MultDigits(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {multDigits}");
