afdc2780be97c628229ec1cbdf5ec711da6efb70
Anton <Cevleron@yandex.ru>
2022-11-28 11:01:21 +0300

    add rast 27

--- Rast27/Program.cs
+++ Rast27/Program.cs
@@ -2,3 +2,22 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 0 )
+{
+    Console.WriteLine("Введите число");
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number < 0) Console.WriteLine("Введите введите целое положительное число");
+}
+
+int num = 0;
+while (number > 0)
+{
+    num = num + number%10;
+    number = number/10;
+    
+}
+Console.WriteLine(num);
