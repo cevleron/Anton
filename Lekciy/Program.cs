2226db0f9c8ca7b5180a3ea029f9c4e4ae53af64
Anton <Cevleron@yandex.ru>
2022-12-01 13:37:29 +0300

    add Rast31

--- Lekciy/Program.cs
+++ Lekciy/Program.cs
@@ -1,33 +1,115 @@
 ﻿
+// int[,] pic = new int[,] 
+// {
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+// {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// };
+
+// // void PrintArray(int[,] matr)
+// // {
+// // for (int i = 0; i < matr.GetLength(0); i++)
+// // {
+
+// //         for (int j = 0; j < matr.GetLength(1); j++)
+// //         {
+// //             Console.Write($"{matr[i,j]} ");
+// //         }
+// // Console.WriteLine() ;
+// // }
+// // }
+
+// // void FillArray(int[,] matr)
+// // {
+// // for (int i = 0; i < matr.GetLength(0); i++)
+// // {
+
+// //         for (int j = 0; j < matr.GetLength(1); j++)
+// //         {
+// //             matr[i,j] = new Random().Next(1,10);
+// //         }
+// // Console.WriteLine() ;
+// // }
+// // }
+
+// // int[,] matrix = new int[3, 4];
+// // PrintArray(matrix);
+// // FillArray(matrix);
+// // PrintArray(matrix);
 
 
-void PrintArray(int[,] matr)
-{
-for (int i = 0; i < matr.GetLength(0); i++)
-{
+// void PrintImage (int[,] image)
+// {
+//     for (int i = 0; i < image.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < image.GetLength(1); j++)
+//         {
+//             if (image[i,j]==0) Console.Write($" ");
+//             else Console.Write($"+");
+//     }
+//     Console.WriteLine();
+//     }
+// }
+// PrintImage(pic);
+
+// void FillImage (int row, int col)
+// {
+//     if(pic[row,col] == 0)
+//     {
+//         pic[row,col] = 1;
+//         FillImage(row-1,col);
+//         FillImage(row,col-1);
+//         FillImage(row+1,col);
+//         FillImage(row,col+1);
+//     }
+// }
+
+// PrintImage(pic);
+// FillImage(13,13);
+// PrintImage(pic);
+
+
+// Рекурсия Факториала
+
+// int Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// Console.WriteLine(Factorial(5));
+
+// Рекурсия Фибоначи
+
+// int Fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return Fibonacci(n-1) + Fibonacci(n-2);
     
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            Console.Write($"{matr[i,j]} ");
-        }
-Console.WriteLine() ;
-}
-}
+// }
+// for (int i = 1; i < 10; i++)
+//     {
+//         Console.WriteLine(Fibonacci(i));
+//     }
 
-void FillArray(int[,] matr)
-{
-for (int i = 0; i < matr.GetLength(0); i++)
-{
-    
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            matr[i,j] = new Random().Next(1,10);
-        }
-Console.WriteLine() ;
-}
-}
 
-int[,] matrix = new int[3, 4];
-PrintArray(matrix);
-FillArray(matrix);
-PrintArray(matrix);
+// 
