93eb3667cd5828a1cfd1e10c8f4afb8e8fc24008
Anton <Cevleron@yandex.ru>
2022-12-16 21:38:26 +0300

    // Задача 46: Задайте двумерный массив размером m×n, // заполненный случайными целыми числами. // m = 3, n = 4. // 1 4 8 19 // 5 -2 33 -2

--- Rast46/Program.cs
+++ Rast46/Program.cs
@@ -5,29 +5,33 @@
 // 5 -2 33 -2
 // 77 3 8 1
 
-int [,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-  int [,] matrix = new int[rows,columns];
+    int[,] matrix = new int[rows, columns];
     Random rnd = new Random();
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0, j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i,j] = rnd.Next(min,max+1);
+            matrix[i, j] = rnd.Next(min, max + 1);
         }
     }
     return matrix;
 }
 
-void PrintArray(double[] arr)
+void PrintMatrix(int[,] matrix)
 {
-    Console.Write("[");
-    for (int i = 0; i < arr.Length; i++)
+   
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        if (i < arr.Length - 1) Console.Write($"{Math.Round(arr[i],2)}, ");
-        else Console.Write($"{Math.Round(arr[i],1)}");
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j],4}, ");
+            else Console.Write($"{matrix[i,j],4}");
+        }
+        Console.WriteLine("]");
     }
-    Console.WriteLine("]");
 }
-double[] array = CreateArrayRndInt(5, 0, 10);
-PrintArray(array);
+int[,] array2D= CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
