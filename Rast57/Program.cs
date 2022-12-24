f53ed658a1a8085a1cbef85f31a1eb40d7704607
Anton <Cevleron@yandex.ru>
2022-12-24 19:04:39 +0300

    Задача 57. Подсчет одинаковых элементов в массиве одномерном

---
+++ Rast57/Program.cs
@@ -0,0 +1,101 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// Набор данных
+// {1, 9, 9, 0, 2, 8, 0, 9 }
+// 1, 2, 3
+// 4, 6, 1
+// 211, 6
+// Частотный массив
+// О встречается 2 раза
+// 1 встречается 1 раз
+// 2 встречается 1 раз
+// 8 встречается 1 раз
+// 9 встречается 3 раза
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[] MetrixToArray(int[,] matrix)
+{
+    int[] arr = new int[matrix.Length];
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            arr[k++] = matrix[i, j];
+        }
+
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("{0,4}", arr[i]);
+        Console.Write((i < arr.Length - 1) ? "," : string.Empty);
+    }
+    Console.Write("]\n");
+}
+
+void CountValue(int[] arr)
+{
+    int count = 1;
+    int num = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+
+        if (arr[i] == num) count++;
+        else 
+        {
+            Console.WriteLine($"{num} встречается {count} раз");
+            count = 1;
+            num = arr[i];
+        }
+         
+    }
+    Console.WriteLine($"{num} встречается {count} раз");
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+Console.WriteLine();
+int[] array = MetrixToArray(array2D);
+Array.Sort(array);
+PrintArray(array);
+Console.WriteLine();
+CountValue(array);
+// PrintArray(array);
+
