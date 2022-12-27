f6ca776f443226274d284df3bc2398ea11019088
Anton <Cevleron@yandex.ru>
2022-12-27 13:12:17 +0300

    Задача 67. Сложение всех элементов натур числа с поощью рекурсии

---
+++ Rast67/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+    if (num != 0)
+    {
+        return num % 10 + SumDigits(num / 10);
+    }
+    return 0;
+}
+
+Console.WriteLine(SumDigits(number));
+
+
+// double Faktorial (int n)
+// {
+//     if (n==1) return 1;
+//     else
+//     return n*Faktorial(n-1);
+// }
