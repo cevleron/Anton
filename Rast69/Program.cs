f0765dfda142fdd79840203bd8d4224400b91ccd
Anton <Cevleron@yandex.ru>
2022-12-27 15:35:56 +0300

    Задача 69. Рекурсия для вычисления А в степени В

--- Rast69/Program.cs
+++ Rast69/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа А и В, и возводит число А в целую степень В с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3^5)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите натуральное число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int DegreeDigits(int num, int degree)
+{
+    if (degree == 0) return 1;
+    return num * DegreeDigits(num, degree - 1);
+}
+
+Console.WriteLine(DegreeDigits(number1, number2));
+
+
+// double Faktorial (int n)
+// {
+//     if (n==1) return 1;
+//     else
+//     return n*Faktorial(n-1);
+// }
