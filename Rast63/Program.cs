47b677e16d933d405a1581b8bd5d0e29b6a38776
Anton <Cevleron@yandex.ru>
2022-12-27 11:54:03 +0300

    Add Rast 63

---
+++ Rast63/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 63: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N=6->"1, 2, 3, 4, 5, 6''
+
+Console.WriteLine("Введите натрульное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    // Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+
+// Способ с условиями
+// void NaturalNumbers(int num)
+// {
+//     if (num > 0)
+//     {
+//         // Console.Write($"{num} ");
+//         NaturalNumbers(num - 1);
+//         Console.Write($"{num} ");
+//     }
+// }
---
+++ Rast65/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 65: Задайте значения М и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от М до N.
+// M = 1; N = 5-> "1, 2, 3,4, 5"
+// M=4; N = 8-> "4, 6, 7,8"
+
+Console.WriteLine("Введите натрульное число");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натрульное число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int num1, int num2)
+{
+    int num = 0;
+    num1 = M;
+    num2 = N;
+    if (num1 < num2) return;
+    Console.Write($"{num1} ");
+    NaturalNumbers(num + 1);
+   
+    else if (num1 > num2) return;
+    Console.Write($"{num2} ");
+    NaturalNumbers(num2-1);
+    
+}
+NaturalNumbers(num1,num2);
