ffd503dcbc318dbde594ade2aeace90834e94657
Anton <Cevleron@yandex.ru>
2022-12-27 09:27:46 +0300

    add rast58

---
+++ Rast58/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+// Метод по созданию двумерного массива.
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+// Метод по созданию двумерного массива.
+int[,] CreateMatrixRndInt2(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+// Метод печатающий двумерный массив.
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],2}, ");
+            else Console.Write($"{matrix[i, j],2} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(2, 2, 1, 9);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[,] matrix2 = CreateMatrixRndInt2(2, 2, 1, 9);
+PrintMatrix(matrix2);
+
+int[,] GenerationMassiv(int[,] matrix, int[,] matrix2)
+{
+    if(matrix.GetLength != matrix2.GetLength)
+    {
+        Console.Write("Умножать матрицы нельзя так как они не соответствуют правилам умножения");
+    }
+    int [,] matrix3 = new int[]
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            
+        }
+    }
+    return default;
+}
